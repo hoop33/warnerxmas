1f342b43b6e8675699ef4ef1b79ce6b19b887750
Rob Warner <rwarner@grailbox.com>
2016-02-22 11:36:24 -0500

    Fix tests

--- tests/views/HomeView.spec.js
+++ tests/views/HomeView.spec.js
@@ -25,11 +25,6 @@ describe('(View) Home', function () {
   beforeEach(function () {
     _spies = {}
     _props = {
-      counter: 0,
-      ...bindActionCreators({
-        doubleAsync: (_spies.doubleAsync = sinon.spy()),
-        increment: (_spies.increment = sinon.spy())
-      }, _spies.dispatch = sinon.spy())
     }
 
     _component = shallowRenderWithProps(_props)
@@ -44,66 +39,7 @@ describe('(View) Home', function () {
     const h1 = TestUtils.findRenderedDOMComponentWithTag(_rendered, 'h1')
 
     expect(h1).to.exist
-    expect(h1.textContent).to.match(/Welcome to the React Redux Starter Kit/)
+    expect(h1.textContent).to.match(/Warner Christmas/)
   })
 
-  it('Should render with an <h2> that includes Sample Counter text.', function () {
-    const h2 = TestUtils.findRenderedDOMComponentWithTag(_rendered, 'h2')
-
-    expect(h2).to.exist
-    expect(h2.textContent).to.match(/Sample Counter/)
-  })
-
-  it('Should render props.counter at the end of the sample counter <h2>.', function () {
-    const h2 = TestUtils.findRenderedDOMComponentWithTag(
-      renderWithProps({ ..._props, counter: 5 }), 'h2'
-    )
-
-    expect(h2).to.exist
-    expect(h2.textContent).to.match(/5$/)
-  })
-
-  it('Should render exactly two buttons.', function () {
-    const wrapper = mount(<HomeView {..._props} />)
-
-    expect(wrapper).to.have.descendants('.btn')
-  })
-
-  describe('An increment button...', function () {
-    let _btn
-
-    beforeEach(() => {
-      _btn = TestUtils.scryRenderedDOMComponentsWithTag(_rendered, 'button')
-        .filter(a => /Increment/.test(a.textContent))[0]
-    })
-
-    it('should be rendered.', function () {
-      expect(_btn).to.exist
-    })
-
-    it('should dispatch an action when clicked.', function () {
-      _spies.dispatch.should.have.not.been.called
-      TestUtils.Simulate.click(_btn)
-      _spies.dispatch.should.have.been.called
-    })
-  })
-
-  describe('A Double (Async) button...', function () {
-    let _btn
-
-    beforeEach(() => {
-      _btn = TestUtils.scryRenderedDOMComponentsWithTag(_rendered, 'button')
-        .filter(a => /Double/.test(a.textContent))[0]
-    })
-
-    it('should be rendered.', function () {
-      expect(_btn).to.exist
-    })
-
-    it('should dispatch an action when clicked.', function () {
-      _spies.dispatch.should.have.not.been.called
-      TestUtils.Simulate.click(_btn)
-      _spies.dispatch.should.have.been.called
-    })
-  })
 })
