ad2afcd22db7b7043291661a2a271f9dfe25fbf7
Rob Warner <rwarner@grailbox.com>
2016-02-27 13:29:19 -0500

    Add tests for schedule

--- tests/redux/modules/schedule.spec.js
+++ tests/redux/modules/schedule.spec.js
@@ -8,5 +8,17 @@ describe('(Redux Module) Schedule', function () {
     it('Should be a function.', function () {
       expect(scheduleReducer).to.be.a('function')
     });
+
+    it('Should have siblings.', function () {
+      expect(scheduleReducer().siblings.length).to.be.greaterThan(0);
+    });
+
+    it('Should have a year greater than 2000.', function () {
+      expect(scheduleReducer().year).to.be.greaterThan(2000);
+    });
+
+    it('Should have a reference year greater than 2000.', function () {
+      expect(scheduleReducer().referenceYear).to.be.greaterThan(2000);
+    });
   });
 });
